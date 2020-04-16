dfccc94e7e534591fbf6378ffa99b78a8caeb133
Wesley Quintana <wesley.quintana@intimetec.com>
2020-04-16 14:55:00 -0600

    fourth attempt at gitignore

--- VFS/VFS.Models/PrivateResources.Designer.cs
+++ VFS/VFS.Models/PrivateResources.Designer.cs
@@ -61,7 +61,7 @@ namespace VFS.Models {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to server=anotherthing.
+        ///   Looks up a localized string similar to server=fourth thing.
         /// </summary>
         internal static string CONNECTION_STRING {
             get {
